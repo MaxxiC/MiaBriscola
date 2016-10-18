49514b0525716534ba707ce5a33a4f3a53f8a325
MaxxiC <andmassic@icloud.com>
2016-10-18 10:40:27 +0200

    Um

---
+++ Conti.Massimiliano.5I.Briscola/Conti.Massimiliano.5I.Briscola/Classi/Carta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Conti.Massimiliano._5I.Briscola
+{
+    class Carta
+    {
+        public string Seme { get; set; }
+        public int Valore { get; set; }
+        public bool Usata { get; set; }
+        
+        public string imgCard { get; set; }
+
+        public BitmapImage percorso { get; set; }
+        
+
+        public Carta(string sm, int vl)
+        {
+            Seme = sm;
+            Valore = vl;
+
+            imgCard = "/Immagini/" + Seme + " (" + Valore.ToString() + ").png";
+            //pack://siteoforigin:,,,
+            percorso = new BitmapImage(new Uri(imgCard, UriKind.Relative));
+        }
+    }
+}
---
+++ Conti.Massimiliano.5I.Briscola/Conti.Massimiliano.5I.Briscola/Classi/Utente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conti.Massimiliano._5I.Briscola
+{
+    class Utente
+    {
+        public int Punteggio { get; set; }
+
+        public List<Carta> MieCarte = new List<Carta>();
+
+
+
+
+
+
+        public Utente()
+        { }
+
+        public Utente(List<Carta> list1)
+        {
+            MieCarte = list1;
+        }
+
+        public void addCarta(Carta cc)
+        {
+            MieCarte.Add(cc);
+        }
+    }
+}
